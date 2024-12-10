06ab71635fb0de06b2751cacb6eef6579a54fa14
ihor-0406 <sevcenkoi461@gmail.com>
2024-12-10 11:48:40 +0100

    Update MultiplicationTable.java

--- homework01/src/homework3/MultiplicationTable.java
+++ homework01/src/homework3/MultiplicationTable.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
 
 	public static void main(String[] args) {
 		System.out.println("Таблица умножения на 5 :");
-		for(int i=5; i<=10; i++) {
+		for(int i=1; i<=10; i++) {
 			System.out.println(i +" x 5 = "+ (i * 5));
 		}
 
